715b20119c7764a85a4f2eb7a7ad8e0124c8791d
Christopher Spooner <christopherspooner423@gmail.com>
2023-06-05 15:09:04 -0500

    update

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import axios from 'axios';
+import {useState} from 'react';
+import {Button} from 'react-bootstrap';
 
 function BestBooks() {
-   
+  const [bookData, setBookData] = useState([]);
+
+   let bookResponse = axios.get(`http://localhost:3001/books`);
+   bookResponse.then(function(res){
+    console.log(res.data);
+    setBookData(res.data)
+   })
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
